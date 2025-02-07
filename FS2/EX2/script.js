a2ec5c3bef496c15196ff062e08c5b2d5943c3fe
Aswin Saathappan A <aswinsaathappan14@gmail.com>
2025-02-07 10:43:22 +0530

    Add files via upload

---
+++ FS2/EX2/script.js
@@ -0,0 +1,49 @@
+/*String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object*/
+
+/*String*/
+var x = "Aswin";
+console.log(x + " is a string");
+
+/*Number withoout decimal*/
+var x = 10;
+console.log(x + " is a number");
+
+/*Number with decimals*/
+var x = 10.01;
+console.log(x + " is a number");
+
+/*Boolean*/
+var x = true;
+console.log(x + " is boolean");
+
+var x = false
+console.log(x + " is a boolean");
+
+/*Undefined*/
+let y;
+console.log(y);
+
+/*Null*/
+var x = null;
+console.log(x);
+
+/*Objects*/
+const obj = {Name:"Aswin",
+             Age:18,
+             gender:"male",
+           }
+console.log(obj);
+
+/*Big Int*/
+var x= BigInt("123456789012345678901234567890");
+console.log(x);
+
+let z = 123e-5;
+console.log(z);
